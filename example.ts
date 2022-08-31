c2ef7c8bbc077085182b3a6b0ef18e63b4d7e3a6
Sydist <sydistbot.personal@gmail.com>
2022-08-31 08:47:07 +0200

    chore: add example.ts as a usage example

---
+++ example.ts
@@ -0,0 +1,6 @@
+import "https://deno.land/std@0.153.0/dotenv/load.ts";
+import { getMessages } from "./get.ts";
+// import { postMessage } from "./post.ts";
+
+// postMessage("815217091254681610", "KYS!!")
+getMessages("815217091254681610")
