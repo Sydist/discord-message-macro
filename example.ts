bcc31af773f57a019a28a43d3d62af385efd6076
Sydist <sydistbot.personal@gmail.com>
2022-09-23 13:53:46 +0200

    chore: move dm id into .env

--- example.ts
+++ example.ts
@@ -2,5 +2,7 @@ import "https://deno.land/std@0.153.0/dotenv/load.ts";
 import { getMessages } from "./get.ts";
 import { postMessage } from "./post.ts";
 
-// postMessage("815217091254681610", "KYS!!")
-// getMessages("815217091254681610")
+const DM_ID = Deno.env.get("DM_ID") as string;
+
+// postMessage(DM_ID, "Message")
+getMessages(DM_ID)
